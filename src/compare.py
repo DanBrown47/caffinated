2e5ae2e56b77457430d5a86f19d6934cb46539dc
abhi-accubits <abhinand@accubits.com>
2021-10-18 10:41:20 +0530

    Update compare.py

--- src/compare.py
+++ src/compare.py
@@ -2,14 +2,24 @@ import json
 
 """
 Comparison Core
-
-Author : Danwand NS github.com/DanBrown47
-
+Author : Danwand NS - github.com/DanBrown47
 The package compares between the API requests and returns the changes
 with help of IO package
 """
 
 class Comparison():
+   
+    def change(data1,data2):
+        data1 = json.dumps(data1)
+        data2 = json.dumps(data2)
+        
+        pass
+    def compare(a,b) -> bool:
+        if(a == b):
+            return True
+        else:
+            change(a,b)
+class Comparison_db_call_notused():
    
     def change(data1,data2):
         data1 = json.dumps(data1)
